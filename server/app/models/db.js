f4e929859855b7bdd0c625484790ebbe042feca2
[your <oliver.tomondy@sigmatek.at>
2021-09-22 15:41:10 +0200

    Deleted database credentials

--- server/app/models/db.js
+++ server/app/models/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: 'sql11.freemysqlhosting.net',
-  user:'sql11439208',
-  password: 'h6VRIjT3HW',
-  database:'sql11439208'
+  host: '',
+  user:'',
+  password: '',
+  database:''
 });
 
 // open the MySQL connection
--- server/app/models/equipment.model.js
+++ server/app/models/equipment.model.js
@@ -1,21 +1,24 @@
 const sql = require("./db.js");
 
-// constructor
-const Equipment = function(equipment) {
-  this.name = 'name';
+ class Equipment {
+
+  constructor() {
+    
+  }
+
+  create(equipment, result) {
+    sql.query("INSERT INTO equipment SET ?", equipment, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+  
+      console.log("created equipment: ", { id: res.insertId, ...equipment });
+      result(null, { id: res.insertId, ...equipment });
+    });
+  };
 };
 
-Equipment.create = (equipment, result) => {
-  sql.query("INSERT INTO equipment SET ?", equipment, (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
 
-    console.log("created equipment: ", { id: res.insertId, ...equipment });
-    result(null, { id: res.insertId, ...equipment });
-  });
-};
-
-module.exports = Equipment;
+module.exports.Equipment = Equipment;

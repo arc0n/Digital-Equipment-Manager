18f3768a9d2406482023f1b68c4420890286cbe1
Pascal <Pascal@DESKTOP-P4R5757>
2021-10-02 12:08:45 +0200

    server: docker setup finished

--- server/app/models/db.js
+++ server/app/models/db.js
@@ -1,17 +1,25 @@
 const mysql = require("mysql2");
 
+const MY_SQL_HOST = process.env.MYSQL_HOST || 'localhost'
+const MY_SQL_PORT = process.env.MYSQL_PORT || '3306'
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: '3307',
+const connection =  mysql.createConnection({
+  host: MY_SQL_HOST,
+  port: MY_SQL_PORT,
   user:'root',
   password: 'root',
-  database:'test_db',
+  database:'equipment_manager',
 });
 
 // open the MySQL connection
 connection.connect(error => {
-  if (error) throw error;
+
+  if (error) {
+    console.log("Used host: ", MY_SQL_HOST)
+    console.log("Used port: ", MY_SQL_PORT)
+    throw error;
+
+  }
   console.log("Successfully connected to the database.");
 });
 

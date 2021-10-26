e02efc827963462027f3657cffef674608e1180e
Martin Burgmann <itsartayon@outlook.com>
2021-10-26 17:51:10 +0200

    further api tests. Adjusted to api changes

--- digital-equipment-manager-ui/cypress/integration/api-tests.spec.js
+++ digital-equipment-manager-ui/cypress/integration/api-tests.spec.js
@@ -23,27 +23,79 @@ describe('api-tests', () => {
     cy.request('http://localhost:3000/item/4z8k9a6bqx7u7ad')
       .then((response) => {
         expect(response).property('status').to.equal(200)
-        cy.wrap(response.body.result.length).should('equal', 1)
-        cy.wrap(response.body.result[0].dynamic_id).should('equal', '4z8k9a6bqx7u7ad')
-        expect(response.body.result[0].dynamic_id).to.equal('4z8k9a6bqx7u7ad')
+        //cy.wrap(response.body.result.length).should('equal', 1)
+        cy.wrap(response.body.result.dynamic_id).should('equal', '4z8k9a6bqx7u7ad')
+        expect(response.body.result.dynamic_id).to.equal('4z8k9a6bqx7u7ad')
       })
 
   })
 
   it('item by invalid id response NOT_FOUND', () => {
 
-    cy.request('http://localhost:3000/item/bullshit')
+    cy.request({
+      url: 'http://localhost:3000/item/bullshit',
+      failOnStatusCode: false,
+    })
       .then((response) => {
-       // expect(response).property('status').to.equal(404)
-        cy.wrap(response.body.message).should('equal', 'NOT_FOUND')
+        expect(response).property('status').to.equal(404)
+        cy.wrap(response.body.result).should('equal', 'NOT_FOUND')
       })
 
   })
 
   it('create new item and check it', () => {
-    let resolveFn;
-    const postOngoing = new Promise((resolve, reject) => {
-      resolveFn = resolve;
+
+    cy.request({
+      url:'http://localhost:3000/item/',
+      method:'POST',
+      body:{
+        "serial_number": "12345",
+        "photo": "/",
+        "description": "test",
+        "status": "aktiv",
+        "item_model_id": 1,
+        "item_type_id": 1,
+      }
+    })
+      .then((response) => {
+        expect(response).property('status').to.equal(200)
+        let createdId = response.body.result.dynamic_id;
+        cy.request({
+          url: 'http://localhost:3000/item/' + response.body.result.dynamic_id,
+          method:'GET',
+        })
+          .then((response) => {
+            expect(response).property('status').to.equal(200)
+            cy.wrap(response.body.result.dynamic_id).should('equal', createdId)
+          })
+      })
+
+  })
+
+  it('try to create item without item_model_id', () => {
+
+    cy.request({
+      url:'http://localhost:3000/item/',
+      method:'POST',
+      failOnStatusCode: false,
+      body:{
+        "serial_number": "12345",
+        "photo": "/",
+        "description": "",
+        "status": "aktiv",
+        //"item_model_id": 1,
+        "item_type_id": 1,
+      }
+    })
+      .then((response) => {
+        expect(response).property('status').to.equal(500)
+        cy.wrap(response.body.result).should('equal', 'INVALID_REQUEST')
+      })
+
+    })
+
+  it('Delete newly created item', () => {
+    const createId = new Promise((resolve, reject) => {
     });
 
     cy.request({
@@ -52,28 +104,58 @@ describe('api-tests', () => {
       body:{
         "serial_number": "12345",
         "photo": "/",
-        "description": "",
+        "description": "test",
         "status": "aktiv",
         "item_model_id": 1,
-        "model_name": "S-12",
-        "item_type": "Schlagstock",
         "item_type_id": 1,
-        "item_type_description": "Ein langes zylindrisches Instrument."
       }
     })
       .then((response) => {
         expect(response).property('status').to.equal(200)
-        cy.wrap(response.body.message).should('equal', 'Item created successfully.')
-        resolveFn(response.body.result.dynamic_id)
-      })
-    postOngoing.then((id)=>{
-      cy.request('http://localhost:3000/item/' + id)
-        .then((response) => {
-          expect(response).property('status').to.equal(200)
-          cy.wrap(response.body.result.length).should('equal', 1)
-          cy.wrap(response.body.result[0].dynamic_id).should('equal', id)
+        let createdId = response.body.result.dynamic_id;
+        cy.request({
+          url:'http://localhost:3000/item/' + response.body.result.dynamic_id,
+          method: 'DELETE',
         })
+          .then((response) => {
+            expect(response).property('status').to.equal(200)
+            cy.wrap(response.body.result).should('equal', createdId)
+          })
+      })
+
+   })
+
+  it('Edit newly created item', () => {
+    const createId = new Promise((resolve, reject) => {
+    });
+
+    cy.request({
+      url:'http://localhost:3000/item/',
+      method:'POST',
+      body:{
+        "serial_number": "12345",
+        "photo": "/",
+        "description": "test",
+        "status": "aktiv",
+        "item_model_id": 1,
+        "item_type_id": 1,
+      }
     })
+      .then((response) => {
+        expect(response).property('status').to.equal(200)
+        cy.request({
+          url:'http://localhost:3000/item/' + response.body.result.dynamic_id,
+          method: 'PUT',
+          body:{
+            "serial_number": "123456",
+          }
+        })
+          .then((response) => {
+            expect(response).property('status').to.equal(200)
+            cy.wrap(response.body.result.serail_number).should('equal', '123456');
+          })
+      })
+
   })
 
 })

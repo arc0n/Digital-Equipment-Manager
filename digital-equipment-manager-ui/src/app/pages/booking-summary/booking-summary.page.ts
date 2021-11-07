c9d8f98b906e5e1960ddfaef0793ba5c6497c365
arcoN <arcon@gmx.at>
2021-11-07 22:26:31 +0100

    Designed Booking Summary Page

--- digital-equipment-manager-ui/src/app/pages/booking-summary/booking-summary.page.ts
+++ digital-equipment-manager-ui/src/app/pages/booking-summary/booking-summary.page.ts
@@ -1,31 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {mergeMap, take} from "rxjs/operators";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Item, Person} from "../../services/model";
-import {forkJoin} from "rxjs";
+import {forkJoin, Subscription} from "rxjs";
 import {ItemResourceService} from "../../services/api-services/item-resource.service";
 import {PersonResourceService} from "../../services/api-services/person-resource.service";
 import {ToastController} from "@ionic/angular";
 import {BookingResourceService} from "../../services/api-services/booking-resource.service";
+import {CommonStateService} from "../../services/common-state.service";
 
 @Component({
   selector: 'app-booking-summary',
   templateUrl: './booking-summary.page.html',
   styleUrls: ['./booking-summary.page.scss'],
 })
-export class BookingSummaryPage implements OnInit {
+export class BookingSummaryPage implements OnInit, OnDestroy {
 
   /** @internal */
   person: Person;
   /** @internal */
   item: Item
+  /** @internal */
+  showMobileMenu = true;
 
   constructor(private route: ActivatedRoute,
               private itemSrv: ItemResourceService,
               private personSrv: PersonResourceService,
               private bookingSrv: BookingResourceService,
               private router: Router,
+              private state: CommonStateService,
               private toastController: ToastController) {
+
+    this.subscriptions.push(
+      this.state.getSplitPaneVisible().subscribe(
+        (isVisible) => this.showMobileMenu = !isVisible
+      )
+    )
+  }
+
+  subscriptions: Subscription[] = [];
+  items: Item[] = [];
+
+  goBack() {
+    window.history.back();
   }
 
   ngOnInit() {
@@ -38,10 +55,10 @@ export class BookingSummaryPage implements OnInit {
           ]
         )
       })
-    ).subscribe(async ([item, person]) => {
-      this.item = item;
+    ).subscribe(async ([items, person]) => {
+      this.items.push(items);
       this.person = person
-      if (!item || !person) {
+      if (!items || !person) {
         const toast = await this.toastController.create({
           position: "bottom", color: "danger",
           message: "Verbindung zum Server verloren, bitte Seite neu laden"
@@ -49,6 +66,10 @@ export class BookingSummaryPage implements OnInit {
         toast.present();
       }
     })
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
 

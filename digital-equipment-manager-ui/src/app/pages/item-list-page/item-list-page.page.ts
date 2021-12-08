634fc7df0ef163bff3d7e091a4118a5bf0679c72
Pascal <Pascal@DESKTOP-P4R5757>
2021-12-08 13:34:07 +0100

    ui: fixed mobile settings navigation

--- digital-equipment-manager-ui/src/app/pages/item-list-page/item-list-page.page.ts
+++ digital-equipment-manager-ui/src/app/pages/item-list-page/item-list-page.page.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {Item} from "../../services/model";
+import {CommonStateService} from "../../services/common-state.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-item-list-page',
   templateUrl: './item-list-page.page.html',
   styleUrls: ['./item-list-page.page.scss'],
 })
-export class ItemListPagePage implements OnInit {
+export class ItemListPagePage implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+  /** @internal */
+  public showMobileMenu: boolean = false;
 
-  constructor(private router: Router) { }
+
+  constructor(public router: Router, public state: CommonStateService) {
+  }
 
   ngOnInit() {
+    this.subscriptions.push(
+      this.state.getSplitPaneVisible().subscribe(
+        (isVisible) => this.showMobileMenu = !isVisible
+      )
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
   itemClicked(event: Item) {
     this.router.navigate(['equipment'], {queryParams: {id: event.dynamic_id}});
   }
+
+
 }

a5dd0527885eef49fec476272ea76c6d5c37d06e
Oliviero50 <oliver.tomondy@gmail.com>
2022-01-06 12:13:42 +0100

    Updated DDL added more descriptions added borrowed items added user table

--- database/DDL.sql
+++ database/DDL.sql
@@ -44,7 +44,15 @@ CREATE TABLE person
 		sex INTEGER NOT NULL,
 		id_card VARCHAR(255) NOT NULL,
       dynamic_id VARCHAR(25) NOT NULL UNIQUE,
-		address_id INTEGER NOT NULL
+		address_id INTEGER NOT NULL     
+   );
+
+CREATE TABLE user
+   (    
+      id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+      username VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      password VARCHAR(255)
    );
 
 
@@ -102,37 +110,47 @@ INSERT INTO item_model (name, model_number, item_type_id) VALUES ("H-12", "12", 
 INSERT INTO item_model (name, model_number, item_type_id) VALUES ("P-117", "117", 4);
 INSERT INTO item_model (name, model_number, item_type_id) VALUES ("Glock", "G17", 5);
 
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12001,"4z8k9a6bqx7u7ad", "equipment_schlagstock.jpeg","some desc", "aktiv",1);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12002,"q6irq09w1aha126", "equipment_schlagstock.jpeg","lorem ipsum", "aktiv",1);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12003,"7zdqatdaohetbl9", "equipment_schlagstock.jpeg","test description which is a bit longer", "aktiv",1);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12004,"jvi6q46zqrns5hv", "equipment_glock.jpeg","", "aktiv",5);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12005,"k9ioxw31r7m5h92", "equipment_glock.jpeg","", "aktiv",5);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12006,"yh63pmq4jq2qa46", "equipment_spray.jpeg","", "aktiv",4);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12007,"grxhy5j72vfiwmw", "equipment_taser.jpeg","", "aktiv",2);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12008,"gqh073b9yrf49oj", "equipment_taser.jpeg","", "aktiv",2);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12009,"c7341j0vum6weq2", "equipment_taser.jpeg","", "aktiv",2);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12010,"okt240f1tqzk1us", "equipment_spray.jpeg","", "aktiv",4);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12011,"bxrnkl15s8dvkv5", "equipment_handschellen.jpeg","", "aktiv",3);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12012,"f9lloij3b9wmz82", "equipment_handschellen.jpeg","", "aktiv",3);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12013,"ifdgzecuseimxci", "","", "aktiv",1);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12014,"nv2kopck0vg6ave", "","", "aktiv",1);
-INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12015,"k8spg6siw36act6", "","", "aktiv",1);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12001,"4z8k9a6bqx7u7ad", "equipment_schlagstock.jpeg","Standard-Schlagstock Reihe S, Länge 90 cm, Stahl", "aktiv",1);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12002,"q6irq09w1aha126", "equipment_schlagstock.jpeg","Standard-Schlagstock Reihe S, Länge 90 cm, Stahl", "aktiv",1);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12003,"7zdqatdaohetbl9", "equipment_schlagstock.jpeg","Standard-Schlagstock Reihe S, Länge 90 cm, Stahl", "aktiv",1);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12004,"jvi6q46zqrns5hv", "equipment_glock.jpeg","Pistole Glock, Kaliber 9mm, Schwarz", "aktiv",5);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12005,"k9ioxw31r7m5h92", "equipment_glock.jpeg","Pistole Glock, Kaliber 9mm, Schwarz", "aktiv",5);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12006,"yh63pmq4jq2qa46", "equipment_spray.jpeg","Pffeferspray Teardrop Standard 500ml", "aktiv",4);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12007,"grxhy5j72vfiwmw", "equipment_taser.jpeg","Taser 120 V, Reihe T, Größe 15cm", "aktiv",2);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12008,"gqh073b9yrf49oj", "equipment_taser.jpeg","Taser 120 V, Reihe T, Größe 15cm", "aktiv",2);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12009,"c7341j0vum6weq2", "equipment_taser.jpeg","Taser 120 V, Reihe T, Größe 15cm", "aktiv",2);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12010,"okt240f1tqzk1us", "equipment_spray.jpeg","Pffeferspray Teardrop Standard 500ml", "aktiv",4);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12011,"bxrnkl15s8dvkv5", "equipment_handschellen.jpeg","Handschellen Robust, Stahl, Größe M", "aktiv",3);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12012,"f9lloij3b9wmz82", "equipment_handschellen.jpeg","Handschellen Robust, Stahl, Größe M", "aktiv",3);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12013,"ifdgzecuseimxci", "equipment_schlagstock.jpeg","Standard-Schlagstock Reihe S, Länge 90 cm, Stahl", "dekommisioniert",1);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12014,"nv2kopck0vg6ave", "equipment_schlagstock.jpeg","Standard-Schlagstock Reihe S, Länge 90 cm, Stahl", "aktiv",1);
+INSERT INTO item(serial_number,dynamic_id, photo, description, status, item_model_id) VALUES (12015,"k8spg6siw36act6", "equipment_schlagstock.jpeg","Standard-Schlagstock Reihe S, Länge 90 cm, Stahl", "aktiv",1);
 
 INSERT INTO address(street, zip, city) VALUES ("Steiner Landstraße 45", 3500, "Stein");
 INSERT INTO address(street, zip, city) VALUES ("Steiner Landstraße 48", 3500, "Stein");
 INSERT INTO address(street, zip, city) VALUES ("Steiner Landstraße 52", 3500, "Stein");
 INSERT INTO address(street, zip, city) VALUES ("Doktor-Karl-Dorrek-Straße 87", 3500, "Stein");
 
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Friedrich", "Fellner", '1944-01-05', 1, "e789g50","68gngbkw875rc6x",1);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Sonja", "Goebels", '1955-10-03', 0, "fdsgfd","bxe7toded6807gn", 2);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Andreas", "Lang", '1981-01-04', 1, "6764587","pldpdfib2qy7stv",3);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Sebastian", "Kogler", '1978-02-10', 1, "785287587","1f55e5pkyzatd5c",4);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Alexander", "Wurst", '1972-03-15', 1, "7868787","cjz7282s5y9ro9d",1);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Joachim", "Strauss", '1974-04-15', 1, "144717","yc48wg669irk369",2);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Peter", "Bauer", '1969-06-18', 1, "1757886","518hf4o6lsnf27m",3);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Roland", "Schmidt", '1966-08-15', 1, "7868767","m4bbclu89ef9x8r",4);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Theresia", "Rosenbauer", '1977-11-05', 0, "1475758","iwgb3zkzbt1jaho",1);
-INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Maria", "Winkelbauer", '1970-01-05', 0, "248789","e4bcon7egqe0d9a",2);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Friedrich", "Fellner", "1944-01-05", 1, "e789g50","68gngbkw875rc6x",1);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Sylvia", "Stern", "1955-10-03", 0, "fdsgfd","bxe7toded6807gn", 2);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Andreas", "Lang", "1981-01-04", 1, "6764587","pldpdfib2qy7stv",3);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Sebastian", "Kogler", "1978-02-10", 1, "785287587","1f55e5pkyzatd5c",4);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Alexander", "Wurst", "1972-03-15", 1, "7868787","cjz7282s5y9ro9d",1);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Joachim", "Strauss", "1974-04-15", 1, "144717","yc48wg669irk369",2);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Peter", "Bauer", "1969-06-18", 1, "1757886","518hf4o6lsnf27m",3);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Roland", "Schmidt", "1966-08-15", 1, "7868767","m4bbclu89ef9x8r",4);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Theresia", "Rosenbauer", "1977-11-05", 0, "1475758","iwgb3zkzbt1jaho",1);
+INSERT INTO person(firstname, lastname, birthdate, sex, id_card, dynamic_id, address_id) VALUES ("Maria", "Winkelbauer", "1970-01-05", 0, "248789","e4bcon7egqe0d9a",2);
 
-INSERT INTO casuality_type(id,name) VALUES ('1', 'Defekt'), ('2', 'Ende der Lebzeit');
-INSERT INTO casuality (id,date,description,status,item_id,casuality_type_id) VALUES ('1', '2021-12-19 18:26:31', 'Herr Fellner hat den Taser unabsichtlich beschädigt.', 'inaktiv', '1', '1'), ('2', '2021-12-19 18:26:31', 'Taser ist kaputt gegangen', 'dekommisioniert', '2', '2')
+INSERT INTO casuality_type(id,name) VALUES ("1", "Defekt"), ("2", "Ende der Lebzeit");
+
+INSERT INTO casuality (id,date,description,status,item_id,casuality_type_id) VALUES ("1", "2021-12-19 18:26:31", "Herr Fellner hat den Taser unabsichtlich beschädigt.", "inaktiv", "3", "1");
+INSERT INTO casuality (id,date,description,status,item_id,casuality_type_id) VALUES ("2", "2021-12-19 18:26:31", "Ist leider komplett kaputt gegangen", "dekommisioniert", "13", "2");
+
+INSERT INTO borrowed_item(datetime_out, datetime_in, item_id, person_id) VALUES ("2021-12-20 08:26:31", "2021-12-20 18:26:00", 1, 1);
+INSERT INTO borrowed_item(datetime_out, datetime_in, item_id, person_id) VALUES ("2021-12-20 08:20:10", "2021-12-20 18:00:42", 2, 2);
+INSERT INTO borrowed_item(datetime_out, datetime_in, item_id, person_id) VALUES ("2021-12-20 08:22:50", "2021-12-20 18:50:40", 5, 3);
+INSERT INTO borrowed_item(datetime_out, datetime_in, item_id, person_id) VALUES ("2021-12-20 08:10:15", "2021-12-20 18:10:25", 6, 4);
+INSERT INTO borrowed_item(datetime_out, datetime_in, item_id, person_id) VALUES ("2021-12-21 08:10:15", NULL, 2, 4);
+
+INSERT INTO user(username, email, password) VALUES ('admin', 'admin@email.com', '');
